c7a84e24b22888fcb7d8eb7c8d8b200f8b44265b
nickstu <nickstu85@gmail.com>
2026-01-21 14:22:37 +0100

    changed posture velocity gain

--- inverse_dynamics.py
+++ inverse_dynamics.py
@@ -41,7 +41,7 @@ class InverseDynamics:
         tasks = ['lfoot', 'rfoot', 'com', 'torso', 'base', 'joints']
         weights   = {'lfoot':  1., 'rfoot':  1., 'com':  1., 'torso': 1., 'base': 1., 'joints': 1.e-2}
         pos_gains = {'lfoot': 10., 'rfoot': 10., 'com':  5., 'torso': 1., 'base': 1., 'joints': 10.  }
-        vel_gains = {'lfoot': 10., 'rfoot': 10., 'com': 10., 'torso': 2., 'base': 2., 'joints': 1.e-1}
+        vel_gains = {'lfoot': 10., 'rfoot': 10., 'com': 10., 'torso': 2., 'base': 2., 'joints': 1.   }
 
         # jacobians
         J = {'lfoot' : self.robot.getJacobian(lsole,        inCoordinatesOf=dart.dynamics.Frame.World()),
